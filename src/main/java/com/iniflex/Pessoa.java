c757a820a811e3eb6a1f483c6a0e640e0f65af60
Marcelo Moreira <mmds12@hotmail.com>
2026-09-15 17:56:16 -0300

    Add Pessoa class with name and birth date attributes

---
+++ src/main/java/com/iniflex/Pessoa.java
@@ -0,0 +1,11 @@
+package com.iniflex;
+import java.time.LocalDate;
+public class Pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome; this.dataNascimento = dataNascimento;
+    }
+    public String getNome() { return nome; }
+    public LocalDate getDataNascimento() { return dataNascimento; }
+}
